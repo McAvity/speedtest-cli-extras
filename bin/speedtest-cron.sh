dc81b305ee1f587d36ff9e114a604ebd2d704001
Marcin Cenkier <marcin@cenkier.com>
2016-05-28 15:40:31 +0200

    detailed data go into speedtest.csv

--- bin/speedtest-cron.sh
+++ bin/speedtest-cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 extras_home=$SPEEDTEST_HOME
 log=$extras_home/speedtest.log
+csv=$extras_home/speedtest.csv
 
 date >> $log
 $extras_home/speedtest-ifttt.sh >> $log
+$extras_home/speedtest-csv --last >> $csv
 echo "" >> $log
